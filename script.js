7e0e75a683f2d17f1e122d37ea1792487c2188f8
Max Souza <maximinosz8@gmail.com>
2022-06-21 15:31:42 -0300

    calculador de idade

---
+++ script.js
@@ -0,0 +1,6 @@
+function idade() {
+    var ano, calculo ;
+    ano = document.getElementById("ano").value ;
+    calculo = 2022 - ano ;
+    document.getElementById("resultado").innerHTML = calculo ; 
+}
